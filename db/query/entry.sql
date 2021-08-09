21092396fe6d0ddd0ba556cf8ead74e3f1afbdd8
pranayhere <pranaybsankpal2050@gmail.com>
2021-08-09 16:00:33 +0530

    queries up to date

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -1,18 +1,24 @@
 -- name: CreateEntry :one
 INSERT INTO entries (wallet_id,
-                     amount)
-VALUES ($1, $2)
-RETURNING *;
+                     amount,
+                     type,
+                     balance,
+                     transfer_id)
+VALUES ($1, $2, $3, $4, $5) RETURNING *;
 
 -- name: GetEntry :one
 SELECT *
 FROM entries
-WHERE id = $1
-LIMIT 1;
+WHERE id = $1 LIMIT 1;
 
 -- name: ListEntries :many
 SELECT *
 FROM entries
 WHERE wallet_id = $1
-ORDER BY id
-LIMIT $2 OFFSET $3;
+ORDER BY id LIMIT $2
+    OFFSET $3;
+
+-- name: GetEntriesByTransferID :many
+SELECT *
+FROM entries
+WHERE transfer_id = $1;
--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -1,20 +1,19 @@
 -- name: CreateTransfer :one
-INSERT INTO transfers (from_wallet_id,
+INSERT INTO transfers (type,
+                       from_wallet_id,
                        to_wallet_id,
                        amount)
-VALUES ($1, $2, $3)
-RETURNING *;
+VALUES ($1, $2, $3, $4) RETURNING *;
 
 -- name: GetTransfer :one
 SELECT *
 FROM transfers
-WHERE id = $1
-LIMIT 1;
+WHERE id = $1 LIMIT 1;
 
 -- name: ListTransfers :many
 SELECT *
 FROM transfers
 WHERE from_wallet_id = $1
    OR to_wallet_id = $2
-ORDER BY id
-LIMIT $3 OFFSET $4;
+ORDER BY id LIMIT $3
+    OFFSET $4;
